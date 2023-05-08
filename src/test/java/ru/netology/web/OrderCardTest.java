f5d5892df30fe09570dd9857c29d53ed54fcac3c
pavel shuvalov <pmshuv975@gmail.com>
2023-05-09 02:25:33 +0300

    positive tests added

---
+++ src/test/java/ru/netology/web/OrderCardTest.java
@@ -0,0 +1,58 @@
+package ru.netology.web;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class OrderCardTest {
+    private WebDriver driver;
+
+    @BeforeAll
+    //Запускается перед всеми тестами
+    static void setUpAll() {
+        System.setProperty("webdriver.chrome.driver", "./driver/win/chromedriver.exe");
+        //WebDriverManager.chromedriver().setup();
+    }
+
+    @BeforeEach
+        //Запускается перед каждым тестовым методом
+    void setUp() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--no-sandbox");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        driver.get("http://localhost:9999");
+    }
+
+    @AfterEach
+        //Закрывается драйвер и обнуляется ссылка
+    void tearDown() {
+        driver.quit();
+        driver = null;
+    }
+
+    @Test
+    void shouldPositiveTest() {
+        driver.findElement(By.cssSelector("[data-test-id=name] input")).sendKeys("Соколов Василий");
+        driver.findElement(By.cssSelector("[data-test-id=phone] input")).sendKeys("+79771254455");
+        driver.findElement(By.cssSelector("[data-test-id=agreement]")).click();
+        driver.findElement(By.className("button__text")).click();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        String actual = driver.findElement(By.cssSelector("[data-test-id=order-success]")).getText().trim();
+        assertEquals(expected, actual);
+    }
+
+
+}
